5ff3ff1d0fd406f3e2d5e4a2fc7e11b20ef0a090
TimeKobra <76175031+TimeKobra@users.noreply.github.com>
2021-02-21 18:39:53 +0100

    Cvicenie 1 - uloha 1.1 a 1.2

--- OOP/src/main/java/org/example/First.java
+++ OOP/src/main/java/org/example/First.java
@@ -3,7 +3,16 @@ package org.example;
 public class First {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        int i = 10;
-        System.out.println(i);
+        int i = 15 , j = 15;
+        if(i>j){
+            System.out.println(1);
+        }
+        else if(i==j){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(-1);
+        }
+
     }
 }
