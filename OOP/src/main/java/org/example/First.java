561ad61efa1800868841a7eb2035e45e39ad2e93
TimeKobra <76175031+TimeKobra@users.noreply.github.com>
2021-02-21 19:26:58 +0100

    Cvicenie 2 - uloha 2.3 POlia2

--- OOP/src/main/java/org/example/First.java
+++ OOP/src/main/java/org/example/First.java
@@ -4,14 +4,8 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class First {
     public static void main(String[] args){
-        int[] pole = new int[5];
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int[] pole =  {10,20,30,40,50};
 
-        for(int i = 0 ; i < 5; i++){
-            System.out.println("Zadaj cislo do pola: ");
-            pole[i] = myObj.nextInt();
-
-        }
         for(int j = 0 ; j < 5; j++){
             System.out.println("Na pozicii " +j+ " je cislo : " + pole[j]);
 
